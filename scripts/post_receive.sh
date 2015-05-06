78617a4c8942499ffa59d6802f88b3d317026c2e
Sascha Rüssel <ruessel@synyx.de>
2015-05-06 19:11:20 +0200

    add initial post_receive.sh to run npm install and grunt post-receive

---
+++ scripts/post_receive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source "$HOME/.bash_profile"
+
+cd ~/dashboard-frontend/
+
+echo -e "running npm install……\n"
+/usr/local/node/node-default/bin/npm install
+if [ $? -eq 0 ];then
+  echo -e "SUCCESSFUL\n"
+else
+  echo -e "FAIL\n"
+fi 
+
+echo -e "running grunt…\n"
+/usr/local/node/node-default/bin/grunt
+if [ $? -eq 0 ];then
+  echo -e "SUCCESSFUL\n"
+else
+  echo -e "FAIL\n"
+fi 
+
+
+
+
