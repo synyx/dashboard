81a587a828754c8183eeb45cc6e8507ec6a0520f
Tobias Schneider <schneider@synyx.de>
2015-05-11 17:48:44 +0200

    add sorting on sources list
    refs #19805

--- app/scripts/services/source-statistics.js
+++ app/scripts/services/source-statistics.js
@@ -9,21 +9,22 @@ define([
         sources: function (sources, providedStringTags) {
 
             //tagName -> {count: #Int , names: [#String]}
-            var tagMap = [];
+            var tags = [];
             sources.forEach(function (source) {
                 source.get('tags').forEach(function (tag) {
                     var sourceName = source.get('header').get('name');
-                    var tagEntry = tagMap[tag];
+                    var tagEntry = tags[tag];
                     if (tagEntry) {
                         tagEntry.count++;
                         tagEntry.names.push(sourceName);
                     }
                     else {
-                        tagMap[tag] = {count: 1, names: [sourceName]};
+                        tags[tag] = {count: 1, names: [sourceName]};
                     }
                 });
             });
 
+            var sortedTags = sortArray(tags);
             var content = '...with the filter <b>' + providedStringTags + '</b><br/><br/>';
 
             if (sources.size() > 0) {
@@ -31,8 +32,8 @@ define([
 
                 content += '<table class="table-small width-full center">';
                 content += '<tr><th>Tag</th><th>Count</th><th>In Sources</generateh></tr>';
-                for (var key in tagMap) {
-                    content += '<tr><td>' + key + '</td><td>' + tagMap[key].count + '</td><td>' + tagMap[key].names.join(' || ') + '</td></tr>';
+                for (var key in sortedTags) {
+                    content += '<tr><td>' + key + '</td><td>' + sortedTags[key].count + '</td><td>' + sortedTags[key].names.join(' || ') + '</td></tr>';
                 }
                 content += '</table>';
             }
@@ -49,4 +50,22 @@ define([
             });
         }
     };
+
+    function sortArray(array) {
+        var tuples = [];
+
+        for (var key in array) {
+            tuples.push([key, array[key]]);
+        }
+        tuples.sort(function (a, b) {
+            return b[1].count - a[1].count;
+        });
+
+        var sortedArray = [];
+        tuples.forEach(function (tuple) {
+            sortedArray[tuple[0]] = tuple[1];
+        });
+
+        return sortedArray;
+    }
 });
